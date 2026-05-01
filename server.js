cc28d691ab501e7ad0c2c633a9ec186c357d1d4d
LEO <yunusovi747@gmail.com>
2026-05-01 18:50:20 +0900

    feat: building simple backent server on nodejs

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 //NodeJs backenti qurmoqchi bo'lsak Expres NestJs bor ekan
+const express = require('express')
+const app = express()
+
+// Traditional Get API 
+// Tayyor ovqatni response sifatida  olib chiqib beradi
+app.get('/', (req, res) => { 
+  res.send('<h1>Hello World!</h1>')
+})
+
+// Rest  API
+//esa maxsulotlarni response sifatida olib chiqib beradi,  Frontenda ovqat tayorlanadi
+app.get('/rest', (req, res) => {
+  res.json({name: "LEO", age: 21, group: "MIT"})
+})
+app.listen(3000, () => {
+  console.log(`Backend Server is running on port 3000`)
+})
